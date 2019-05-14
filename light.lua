d771121f8c58aa9388e3efd9bc3a6195ba15d0a9
Stanislav Popov <popstas@gmail.com>
2019-05-14 20:37:22 +0500

    fix: light adjustment

--- light.lua
+++ light.lua
@@ -1,8 +1,8 @@
-return function ()
-    local min = 2950
-    local max = 3800
+ return function ()
+    local min = 0
+    local max = 1024
     local total = max - min
-    local a = adc.readvdd33(0);
+    local a = adc.read(0);
     local light = math.floor((a - min) / total * 100)
     print("light:", light)
     mqttClient:publish('light', light)
